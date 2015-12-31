b54958b46d026c8f5f810bdd1946e255eadbc60f
Zeke Sikelianos <zeke@sikelianos.com>
2015-12-30 21:33:43 -0600

    all the things

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const handlebars = require('handlebars')
+const template = "{{#each letters}} {{this}} {{/each}}"
+var context
+
+class Alphabet {
+  get letters() {
+    return 'abc'.split('')
+  }
+}
+
+context = new Alphabet()
+console.log('class instance with getter methods:')
+console.log(handlebars.compile(template)(context))
+
+
+context = {
+  letters: 'xyz'.split('')
+}
+
+console.log('regular object:')
+console.log(handlebars.compile(template)(context))
